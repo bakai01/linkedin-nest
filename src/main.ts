bdbacef00967d6046e4dec37057adc837e5f3523
Bakai <thedesport20@gmail.com>
2021-11-06 17:22:46 +0600

    init commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const startApp = async () => {
+  const PORT = process.env.PORT || 4000;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+};
+
+startApp();
